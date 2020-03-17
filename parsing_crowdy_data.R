e2616b1845d97d544b9bdf400b09dfbb4642eb36
Sama Winder <sgwinder@uw.edu>
2020-03-17 12:15:01 -0700

    Modified for the 20180928 deployment

--- parsing_crowdy_data.R
+++ parsing_crowdy_data.R
@@ -6,11 +6,18 @@ library(tidyverse)
 library(googlesheets4)
 library(lubridate)
 
+setwd("~/Documents/Crowdy/")
+
+# change id and deploystamp depending on which set of data you are working from
+
 # Starting with the most recent pull
 # Crowdy20191203_Data_Cleaned
-# change id and deploystamp depending on which set of data you are working from
-crowdyid <- "1Nw79vxKjTTDp-0ots3wwZoq4NV5Y2wx4cNZk_9vW7KE"
-deploystamp <- "Crowdy20191203"
+#crowdyid <- "1Nw79vxKjTTDp-0ots3wwZoq4NV5Y2wx4cNZk_9vW7KE"
+#deploystamp <- "Crowdy20191203"
+
+# deployed20180928
+crowdyid <- "1UeJjCead3qXnKQecQVoJ2vrP0kf0r6zoIph4LTDY5RU"
+deploystamp <- "20180928"
 
 crowdy_data <- read_sheet(crowdyid, col_types = "c", na = c("", "NA")) # slow
 
@@ -33,6 +40,73 @@ crowdy_wide <- no_nas %>%
   #rowid_to_column() %>%
   spread(key = lastquestion, value = bodyclean)
 
+## TODO: Figure out how to combine rows for convos that went on for more than one day.
+##  There are some (unsuccessful) attempts at this at the end of the script 
+
+# write out crowdy_wide
+#write_csv(crowdy_wide, paste0("data/Convos_", deploystamp, ".csv"))
+
+#### Parse and clean counts
+crowdy_cnts <- crowdy_wide %>%
+  select(from, to, date, starts_with("Cnt"), starts_with("Vehic"))
+crowdy_cnts
+
+# Let's separate out first, second and third counts
+first <- crowdy_cnts %>%
+  select(to, CntDate, CntTime, VehicCount) %>%
+  filter(!is.na(VehicCount))
+first
+
+# note that CntDate is commented out here because the 20180928 Deployment had no CntDate2
+second <- crowdy_cnts %>%
+  filter(!is.na(VehicCount2)) %>%
+  mutate(#CntDate = if_else(!is.na(CntDate2), CntDate2, CntDate),
+         CntTime = CntTime2,
+         VehicCount = VehicCount2) %>%
+  select(to, CntDate, CntTime, VehicCount)
+
+# no third counts for 20180928 deployment
+third <- crowdy_cnts %>%
+  filter(!is.na(VehicCount3)) %>%
+  mutate(#CntDate = if_else(!is.na(CntDate3), CntDate3, CntDate),
+         CntTime = CntTime3,
+         VehicCount = VehicCount3) %>%
+  select(to, CntDate, CntTime, VehicCount)
+
+# bind em together
+counts <- bind_rows(first, second)#, third)
+
+## Now use the joinkey to convert phone numbers to trails
+th_joinkey <- th_info %>%
+  select(TrlNum = Number, cell = `Cell number`) %>%
+  mutate(cell = paste0("+1", str_replace_all(cell, "-", "")))
+
+counts_tr <- counts %>%
+  left_join(th_joinkey, by = c("to" = "cell")) %>%
+  select(TrlNum, everything(), -to) %>%
+  mutate(source = "SMS Volunteer") %>%
+  arrange(CntDate)
+
+counts_tr
+# write it out
+#write_csv(counts_tr, paste0("data/Parking_Counts_", deploystamp, ".csv"))
+
+#### Pull out data on party people, party vehicles, and how long for parking model
+trips <- crowdy_wide %>%
+  select(from, to, date, CntDate, HowLong, PartyPeople, PartyVehics) %>%
+  filter(!(is.na(HowLong) & is.na(PartyPeople) & is.na(PartyVehics))) 
+
+# bind on to trails & remove phone numbers
+trips_tr <- trips %>%
+  left_join(th_joinkey, by = c(to = "cell")) %>%
+  select(-from, -to)
+trips_tr
+
+# write it out
+write_csv(trips_tr, paste0("data/Trips_info_", deploystamp, ".csv"))
+
+
+############ Old ###########
 #### What about convos that go over multiple days?
 # can I combine some of them into single convos?
 crowdy_wide %>%
@@ -49,7 +123,7 @@ multi_dates <- crowdy_wide %>%
             maxdate = max(ymd(CntDate), na.rm = T),
             nadates = sum(is.na(CntDate))) %>%
   filter(n > 1)
-  
+
 # and, selecting out those rows
 multi_nums <- multi_dates %>% 
   ungroup() %>%
@@ -66,62 +140,3 @@ crowdy_wide %>%
 # At least some of these are because a converstaion stretched over multiple days
 
 #### Meh. Let's leave it for now - the to, from, date grouping should be good enough
-
-# write out crowdy_wide
-#write_csv(crowdy_wide, paste0("Convos_", deploystamp, ".csv"))
-
-#### Parse and clean counts
-crowdy_cnts <- crowdy_wide %>%
-  select(from, to, date, starts_with("Cnt"), starts_with("Vehic"))
-crowdy_cnts
-
-# Let's separate out first, second and third counts
-first <- crowdy_cnts %>%
-  select(to, CntDate, CntTime, VehicCount) %>%
-  filter(!is.na(VehicCount))
-first
-
-second <- crowdy_cnts %>%
-  filter(!is.na(VehicCount2)) %>%
-  mutate(CntDate = if_else(!is.na(CntDate2), CntDate2, CntDate),
-         CntTime = CntTime2,
-         VehicCount = VehicCount2) %>%
-  select(to, CntDate, CntTime, VehicCount)
-
-third <- crowdy_cnts %>%
-  filter(!is.na(VehicCount3)) %>%
-  mutate(#CntDate = if_else(!is.na(CntDate3), CntDate3, CntDate),
-         CntTime = CntTime3,
-         VehicCount = VehicCount3) %>%
-  select(to, CntDate, CntTime, VehicCount)
-
-# bind em together
-counts <- bind_rows(first, second, third)
-
-## Now use the joinkey to convert phone numbers to trails
-th_joinkey <- th_info %>%
-  select(TrlNum = Number, cell = `Cell number`) %>%
-  mutate(cell = paste0("+1", str_replace_all(cell, "-", "")))
-
-counts_tr <- counts %>%
-  left_join(th_joinkey, by = c("to" = "cell")) %>%
-  select(TrlNum, everything(), -to) %>%
-  mutate(source = "SMS Volunteer") %>%
-  arrange(CntDate)
-
-counts_tr
-# write it out
-#write_csv(counts_tr, paste0("~/Documents/Crowdy/Parking_Counts_", deploystamp, ".csv"))
-
-#### Pull out data on party people, party vehicles, and how long for parking model
-trips <- crowdy_wide %>%
-  select(from, to, date, CntDate, HowLong, PartyPeople, PartyVehics) %>%
-  filter(!(is.na(HowLong) & is.na(PartyPeople) & is.na(PartyVehics))) 
-
-# bind on to trails & remove phone numbers
-trips_tr <- trips %>%
-  left_join(th_joinkey, by = c(to = "cell")) %>%
-  select(-from, -to)
-
-# write it out
-#write_csv(trips_tr, paste0("Trips_info_", deploystamp, ".csv"))
