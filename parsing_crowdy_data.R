64e9f1588bfb721495c8b6f8bdd7c62c31a3468e
Sama Winder <sgwinder@uw.edu>
2020-03-17 11:41:55 -0700

    Added code for writing out details about trip length and ppl/veh
    Also attempted combining rows across dates, but gave up

--- parsing_crowdy_data.R
+++ parsing_crowdy_data.R
@@ -4,6 +4,7 @@
 
 library(tidyverse)
 library(googlesheets4)
+library(lubridate)
 
 # Starting with the most recent pull
 # Crowdy20191203_Data_Cleaned
@@ -21,16 +22,52 @@ th_info <- read_sheet(joinid, sheet = "Crowdsource Parking Lot Counts TH")
 # spread data 
 
 no_nas <- crowdy_data %>%
-  filter(!is.na(bodyclean)) #
+  filter(!is.na(bodyclean)) 
 
+# note that this recreates NAs, but deleting them first allows for a neat spread
 crowdy_wide <- no_nas %>%
   select(bodyclean, lastquestion, from, to, date) %>%
   #rowid_to_column() %>%
   spread(key = lastquestion, value = bodyclean)
+
+#### What about convos that go over multiple days?
+# can I combine some of them into single convos?
 crowdy_wide %>%
-  filter(is.na(CntTime))
+  group_by(from, to)
+# I have 1,134 rows, and 1,045 unique pairs of to and from
+
+# can I find sequential dates for those duplicate sharers?
+
+# let's select only rows which have duplicate from/to pairs
+multi_dates <- crowdy_wide %>%
+  group_by(from, to) %>%
+  summarise(n = n(),
+            mindate = min(ymd(CntDate), na.rm = T), 
+            maxdate = max(ymd(CntDate), na.rm = T),
+            nadates = sum(is.na(CntDate))) %>%
+  filter(n > 1)
+  
+# and, selecting out those rows
+multi_nums <- multi_dates %>% 
+  ungroup() %>%
+  mutate(tofrom = str_c(to, from)) %>%
+  select(tofrom)
+
+multi_rows <- crowdy_wide %>%
+  mutate(tofrom = str_c(to, from)) %>%
+  filter(tofrom %in% multi_nums$tofrom) %>%
+  arrange(from, to, date)
+
+crowdy_wide %>%
+  filter(is.na(CntDate))
 # At least some of these are because a converstaion stretched over multiple days
 
+#### Meh. Let's leave it for now - the to, from, date grouping should be good enough
+
+# write out crowdy_wide
+#write_csv(crowdy_wide, "convos_20191203.csv")
+
+#### Parse and clean counts
 crowdy_cnts <- crowdy_wide %>%
   select(from, to, date, starts_with("Cnt"), starts_with("Vehic"))
 crowdy_cnts
@@ -72,3 +109,16 @@ counts_tr <- counts %>%
 counts_tr
 # write it out
 #write_csv(counts_tr, "~/Documents/Crowdy/Parking_Counts_Crowdy20191203.csv")
+
+#### Pull out data on party people, party vehicles, and how long for parking model
+trips <- crowdy_wide %>%
+  select(from, to, date, CntDate, HowLong, PartyPeople, PartyVehics) %>%
+  filter(!(is.na(HowLong) & is.na(PartyPeople) & is.na(PartyVehics))) 
+
+# bind on to trails & remove phone numbers
+trips_tr <- trips %>%
+  left_join(th_joinkey, by = c(to = "cell")) %>%
+  select(-from, -to)
+
+# write it out
+#write_csv(trips_tr, "Trips_info_crowdy20191203.csv")
